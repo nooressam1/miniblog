268bf0212d7eb57a993c8fef3334c30384275c0b
Noor <nooressamjx2005@gmail.com>
2025-06-09 18:17:58 +0300

    added color change to buttons

--- src/Modules/Account/Components/PostFilter.jsx
+++ src/Modules/Account/Components/PostFilter.jsx
@@ -7,7 +7,11 @@ const PostFilter = () => {
     <div className="w-[95%] flex flex-col gap-3">
       <div className="flex  space-x-3">
         <button
-          className="rounded-md  md:w-28 capitalize p-2 hover:bg-[#a92dad] bg-[#A30BA8] flex justify-center items-center"
+          className={`rounded-md  md:w-28 capitalize  p-2  flex justify-center items-center ${
+            filterChoice === "All"
+              ? " hover:bg-[#a92dad] bg-[#A30BA8]"
+              : "bg-[#7E96F6]  "
+          }`}
           onClick={() => setFilterChoice("All")}
         >
           <h1 className="text-white   font-medium text-sm md:text-base ">
@@ -15,15 +19,23 @@ const PostFilter = () => {
           </h1>
         </button>{" "}
         <button
-          className="rounded-md  md:w-28 capitalize p-2 hover:bg-[#a92dad] bg-[#A30BA8] flex justify-center items-center"
+          className={`rounded-md  md:w-28 capitalize  p-2  flex justify-center items-center ${
+            filterChoice === "Images"
+              ? " hover:bg-[#a92dad] bg-[#A30BA8]"
+              : "bg-[#7E96F6]  "
+          }`}
           onClick={() => setFilterChoice("Images")}
         >
           <h1 className="text-white   font-medium text-sm md:text-base ">
             Images
           </h1>
-        </button>{" "}
+        </button>
         <button
-          className="rounded-md  md:w-28 capitalize p-2 hover:bg-[#a92dad] bg-[#A30BA8] flex justify-center items-center"
+          className={`rounded-md  md:w-28 capitalize  p-2  flex justify-center items-center ${
+            filterChoice === "Pages"
+              ? " hover:bg-[#a92dad] bg-[#A30BA8]"
+              : "bg-[#7E96F6]  "
+          }`}
           onClick={() => setFilterChoice("Pages")}
         >
           <h1 className="text-white   font-medium text-sm md:text-base ">
