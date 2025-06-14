87a1671516f6750652627f7ec4dafbc7227e0eed
Noor <nooressamjx2005@gmail.com>
2025-06-14 03:27:37 +0300

    added homepage navigation

--- src/Modules/Shared/Components/Navbar.jsx
+++ src/Modules/Shared/Components/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { IconSearch } from "@tabler/icons-react";
 import { IconUserCircle } from "@tabler/icons-react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="w-full h-24 pl-8 pr-5 p-2">
       <div className="flex w-full h-full justify-between items-center">
-        <h1 className="text-[#CFD9FC] font-bold text-2xl">EmberPages</h1>
+        <Link to="/">
+          <h1 className="text-[#CFD9FC] font-bold text-2xl">EmberPages</h1>
+        </Link>
         <div className="grid h-full  grid-cols-[50px_1fr] justify-center items-center">
           <div className="h-2/5 w-2/3">
             <button className="bg-[#B36ABE] rounded-md  flex w-full h-full text-center justify-center items-center">
